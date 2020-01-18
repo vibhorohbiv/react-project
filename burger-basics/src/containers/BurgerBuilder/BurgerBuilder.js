eaba43f1f071facac07b6588b746bee44ff10498
Vibhor Jain <mynameisvibhorjain@gmail.com>
2020-01-18 13:10:12 +0530

    first axios call

--- burger-basics/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-basics/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from  '../../axiosOrders'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -81,7 +82,23 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Vibhor Jain',
+                address: {
+                    street: 'Tavakere',
+                    zipcode: '560029',
+                    country: 'India'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('orders.json', order)
+            .then(response => { console.log(response)})
+            .catch(error => {console.log(error)})
     }
 
     render () {
